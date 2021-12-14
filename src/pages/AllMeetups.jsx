ff9767b0381aaf0be16339ca1afc08b86c1e53b5
renanmartins1813 <renanmartins1813@github.com>
2021-12-14 02:01:17 -0500

    fetch get para mostrar os itens na AllMeetups.jsx

--- src/pages/AllMeetups.jsx
+++ src/pages/AllMeetups.jsx
@@ -1,30 +1,69 @@
 import MeetupList from '../components/meetups/MeetupList';
+import { useState, useEffect } from 'react';
 
-const DUMMY_DATA = [
-    {
-        id: 'm1',
-        title: 'This is a first meetup',
-        image:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description: 'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-    {
-        id: 'm2',
-        title: 'This is a second meetup',
-        image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-];
+// const DUMMY_DATA = [
+//     {
+//         id: 'm1',
+//         title: 'This is a first meetup',
+//         image:
+//             'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+//         address: 'Meetupstreet 5, 12345 Meetup City',
+//         description: 'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+//     },
+//     {
+//         id: 'm2',
+//         title: 'This is a second meetup',
+//         image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+//         address: 'Meetupstreet 5, 12345 Meetup City',
+//         description:'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+//     },
+// ];
+
 
 function AllMeetupsPage() {
-    return (
-        <section>
-            <h1>All Meetups</h1>
-            <MeetupList meetups={DUMMY_DATA} />
-        </section>
-    );
+    const url = 'https://acareactcrashcourse-default-rtdb.firebaseio.com/'
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+    useEffect( ()=>{
+        setIsLoading(true);
+        fetch(
+            `${url}meetup.json`
+        ).then(response =>{
+            return response.json();
+        }).then(data =>{
+            const meetups = [];
+
+            for(const key in data){
+                const meetup = {
+                    id: key,
+                    ...data[key]
+                };
+
+                meetups.push(meetup);
+            }
+            setIsLoading(false);
+            setLoadedMeetups(meetups);
+        })
+    }, [setIsLoading]);
+
+    
+
+    if (isLoading){
+        return(
+            <section>
+                <p>Loading...</p>
+            </section>
+        );
+    }
+    else{
+        return (
+            <section>
+                <h1>All Meetups</h1>
+                <MeetupList meetups={loadedMeetups} />
+            </section>
+        );
+    }
 }
 
 export default AllMeetupsPage;
